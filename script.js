f54f0e3092f73e1d88b508f9d3fb8fa0fdf11431
Greg Schuman <gcschuman@hotmail.com>
2020-09-22 20:25:44 -0700

    Added variable declarations, word bank, word, score, and time initializers, difficulty settings, focusing on the text, start countdown, and generate random word from array.

---
+++ script.js
@@ -0,0 +1,61 @@
+const word = document.getElementById('word');
+const text = document.getElementById('text');
+const scoreEl = document.getElementById('score');
+const timeEl = document.getElementById('time');
+const endgameEl = document.getElementById('end-game-container');
+const settingsBtn = document.getElementById('settings-btn');
+const settings = document.getElementById('settings');
+const settingsForm = document.getElementById('settings-form');
+const difficultySelect = document.getElementById('difficulty');
+
+// List of words for game
+const words = [
+    'sigh',
+    'tense',
+    'airplane',
+    'ball',
+    'pies',
+    'juice',
+    'warlike',
+    'bad',
+    'north',
+    'dependent',
+    'steer',
+    'silver',
+    'highfalutin',
+    'superficial',
+    'quince',
+    'eight',
+    'feeble',
+    'admit',
+    'drag',
+    'loving'
+  ];
+
+//Initialize word
+let randomWord;
+
+//Initialize score
+let score = 0;
+
+//Initialize time
+let time = 10;
+
+//Set difficulty to value in ls or medium
+let difficulty =
+    localStorage.getItem('difficulty') !== null
+    ? localStorage.getItem(difficulty)
+    : medium;
+
+//Focus on text at the start
+text.focus();
+
+//Start countdown
+const timeInterval = setInterval(updateTime, 1000)
+
+//Generate random word from array
+function getRandomWord(){
+    randomWord = getRandomWord();
+    word.innerHTML = randomWord
+}
+
